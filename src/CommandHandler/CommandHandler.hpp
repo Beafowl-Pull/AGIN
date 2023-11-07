d2dc847c71e3f9088f95d8214521e39ee3210671
Clang Robot <robot@uwuclub.com>
2023-11-07 13:00:17 +0000

    :art: Committing clang-format changes

--- src/CommandHandler/CommandHandler.hpp
+++ src/CommandHandler/CommandHandler.hpp
@@ -9,20 +9,20 @@
 #include <utility>
 
 namespace pbrain {
-    class commandHandler {
-        public:
-            static commandHandler& getInstance() {
-                static commandHandler instance("command");
-                return instance;
-            }
-            commandHandler() = default;
-            explicit commandHandler(std::string command) : command(std::move(command)) {};
-            ~commandHandler() = default;
-            static void checkCommand(const std::string &command);
-        private:
-            std::string command;
-    };
-}
+class commandHandler {
+public:
+  static commandHandler &getInstance() {
+    static commandHandler instance("command");
+    return instance;
+  }
+  commandHandler() = default;
+  explicit commandHandler(std::string command) : command(std::move(command)){};
+  ~commandHandler() = default;
+  static void checkCommand(const std::string &command);
 
+private:
+  std::string command;
+};
+} // namespace pbrain
 
-#endif //AGIN_COMMANDHANDLER_HPP
+#endif // AGIN_COMMANDHANDLER_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,14 @@
 // Created by beafowl on 07/11/23.
 //
 
+#include "CommandHandler.hpp"
 #include <iostream>
 #include <string>
-#include "CommandHandler.hpp"
 
 int main() {
-    std::string s;
-    while (std::cin >> s) {
-        pbrain::commandHandler::getInstance().checkCommand(s);
-    }
-    return 0;
+  std::string s;
+  while (std::cin >> s) {
+    pbrain::commandHandler::getInstance().checkCommand(s);
+  }
+  return 0;
 }
