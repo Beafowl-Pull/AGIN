4a5c2ecf4e17ed7d5ef0427b62768c34e1023eb6
Clang Robot <robot@uwuclub.com>
2023-11-08 13:08:56 +0000

    :art: Committing clang-format changes

--- src/CommandHandler/CommandHandler.cpp
+++ src/CommandHandler/CommandHandler.cpp
@@ -13,15 +13,13 @@
 namespace pbrain {
     void CommandHandler::checkCommand(const std::string &command)
     {
-        std::unordered_map<std::string, std::function<void()>> commands = {
-            {"START", [command] {
-                try {
-                    CommandHandler::startGame(command);
-                } catch (std::invalid_argument &e) {
-                    throw pbrain::Error(e.what());
-                }
-            }}
-        };
+        std::unordered_map<std::string, std::function<void()>> commands = {{"START", [command] {
+                                                                                try {
+                                                                                    CommandHandler::startGame(command);
+                                                                                } catch (std::invalid_argument &e) {
+                                                                                    throw pbrain::Error(e.what());
+                                                                                }
+                                                                            }}};
         std::string parsedCommand = command.substr(0, command.find(' '));
 
         if (commands.find(parsedCommand) != commands.end()) {
