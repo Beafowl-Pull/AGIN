57e0b62b89731e5ba40469ad10d2a33114e8a928
Clang Robot <robot@uwuclub.com>
2023-11-08 12:20:16 +0000

    :art: Committing clang-format changes

--- src/CommandHandler/CommandHandler.cpp
+++ src/CommandHandler/CommandHandler.cpp
@@ -2,40 +2,40 @@
 // Created by beafowl on 07/11/23.
 //
 
+#include "CommandHandler.hpp"
 #include <functional>
 #include <iostream>
-#include "CommandHandler.hpp"
+#include "Error.hpp"
+#include "Values.hpp"
 #include <type_traits>
 #include <unordered_map>
-#include "Values.hpp"
-#include "Error.hpp"
 
 namespace pbrain {
     void commandHandler::checkCommand(const std::string &command)
     {
-        std::unordered_map<std::string, std::function<void()>> commands = {
-            {"START", [command, this] {
-                try {
-                    commandHandler::startGame(command);
-                } catch (std::invalid_argument &e) {
-                    throw pbrain::Error(e.what());
-                }
-            }},
-            {"TURN", [command, this] {
-                try {
-                    commandHandler::doTurn(command);
-                } catch (std::invalid_argument &e) {
-                    throw pbrain::Error(e.what());
-                }
-            }},
-            {"BEGIN", [command, this] {
-                try {
-                    commandHandler::doBegin();
-                } catch (std::invalid_argument &e) {
-                    throw pbrain::Error(e.what());
-                }
-            }}
-        };
+        std::unordered_map<std::string, std::function<void()>> commands = {{"START",
+                                                                            [command, this] {
+                                                                                try {
+                                                                                    commandHandler::startGame(command);
+                                                                                } catch (std::invalid_argument &e) {
+                                                                                    throw pbrain::Error(e.what());
+                                                                                }
+                                                                            }},
+                                                                           {"TURN",
+                                                                            [command, this] {
+                                                                                try {
+                                                                                    commandHandler::doTurn(command);
+                                                                                } catch (std::invalid_argument &e) {
+                                                                                    throw pbrain::Error(e.what());
+                                                                                }
+                                                                            }},
+                                                                           {"BEGIN", [command, this] {
+                                                                                try {
+                                                                                    commandHandler::doBegin();
+                                                                                } catch (std::invalid_argument &e) {
+                                                                                    throw pbrain::Error(e.what());
+                                                                                }
+                                                                            }}};
         std::string parsedCommand = command.substr(0, command.find(' '));
 
         if (commands.find(parsedCommand) != commands.end()) {
@@ -61,12 +61,14 @@ namespace pbrain {
         }
     }
 
-    void commandHandler::doTurn(const std::string &command) const {
+    void commandHandler::doTurn(const std::string &command) const
+    {
         if (!_gameStarted) {
             throw std::invalid_argument("Game not started");
             return;
         }
-        std::string x = command.substr(command.find(' ') + 1, command.find(' ', command.find(' ') + 1) - command.find(' ') - 1);
+        std::string x =
+            command.substr(command.find(' ') + 1, command.find(' ', command.find(' ') + 1) - command.find(' ') - 1);
         std::string y = command.substr(command.find(' ', command.find(' ') + 1) + 1);
         std::cout << x << " " << y << std::endl;
     }
